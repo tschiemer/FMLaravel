b6592093214588387e7fc07dbe9b593d95ab9c2d
filou <philip.tschiemer@gmail.com>
2016-06-14 12:14:39 +0200

    comment

--- src/FMLaravel/Database/FileMaker/RecordImplementation.php
+++ src/FMLaravel/Database/FileMaker/RecordImplementation.php
@@ -7,6 +7,10 @@ class RecordImplementation extends FileMaker_Record_Implementation
 
     public function getAllFields()
     {
+        // fields are originally saved as numerically indexed arrays (because of repetition fields), even if it is
+        // only a normal field. So for any array consisting of only one element, assume it's not a repetition field
+        // and just set the first elements value as actual value, otherwise (when it's a repetition field) keep
+        // the array
         return array_map(function ($v) {
             return count($v) > 1 ? $v : reset($v);
         }, $this->_fields);
